32b365ad7e10936c85a089280ece87eb525d6e69
barmajiyat-net <git@barmajiyat.net>
2021-11-19 15:03:58 +0200

    initial commit

---
+++ barmajiyatjs.js
@@ -0,0 +1,40 @@
+/*!
+ * Developed by Barmajiyat.net team
+ * https://barmajiyat.net
+ * Nov 19, 2021
+ */
+
+function getRandomNumber(min, max, decimalPlaces) {
+  return (Math.random() * max + min + Math.random()).toFixed(decimalPlaces);
+}
+
+function getRandomInt(min, max) {
+  return getRandomNumber(min, max, 0);
+}
+
+
+function getURLParams(url) {
+  var search = new URL(url).search;
+  var searchParams = new URLSearchParams(search);
+  return Object.fromEntries(searchParams.entries());
+}
+
+function getCurrentURLParams() {
+  var search = new URL(location.href).search;
+  var searchParams = new URLSearchParams(search);
+  return Object.fromEntries(searchParams.entries());
+}
+
+function getURLParam(paramName, url) {
+  var search = new URL(url).search;
+  var searchParams = new URLSearchParams(search);
+  var value = null;
+  if (searchParams.has(paramName)) {
+    value = searchParams.get(paramName);
+  }
+  return value;
+}
+
+function getParamFromCurrentURL(paramName) {
+  return getURLParam(paramName, location.href);
+}
